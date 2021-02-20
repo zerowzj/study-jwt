c66d7700d0a8941ffbe68787e322d9bc3f994e14
wangzhenjun3 <wangzhenjun3@xdf.cn>
2021-02-20 10:44:31 +0800

    更新代码

--- src/main/java/study/jwt/springboot/support/exception/GlobalErrController.java
+++ src/main/java/study/jwt/springboot/support/exception/GlobalErrController.java
@@ -17,10 +17,10 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
- * 1.注解@ControllerAdvice方式只能处理控制器抛出的异常。此时请求已经进入控制器中。
- * 2.类ErrorController方式可以处理所有的异常，包括未进入控制器的错误，比如404,401等错误
- * 3.如果应用中两者共同存在，则@ControllerAdvice方式处理控制器抛出的异常，类ErrorController方式未进入控制器的异常。
- * 4.@ControllerAdvice方式可以定义多个拦截方法，拦截不同的异常类，并且可以获取抛出的异常信息，自由度更大
+ * 1.注解 @ControllerAdvice 方式只能处理控制器抛出的异常，此时请求已经进入控制器中
+ * 2.类 ErrorController 方式可以处理所有的异常，包括未进入控制器的错误，比如404,401等错误
+ * 3.如果应用中两者共同存在，则 @ControllerAdvice 方式处理控制器抛出的异常，类 ErrorController 方式未进入控制器的异常
+ * 4.@ControllerAdvice 方式可以定义多个拦截方法，拦截不同的异常类，并且可以获取抛出的异常信息，自由度更大
  */
 @Slf4j
 @Controller
@@ -47,7 +47,9 @@ class GlobalErrController implements ErrorController {
     public Result error(HttpServletRequest request) {
         int statusCode = (int) request.getAttribute(KEY_STATUS_CODE);
         log.info("status_code= {}", statusCode);
-
+        ServletWebRequest requestAttributes = new ServletWebRequest(request);
+        Map<String, Object> errAttr = errorAttributes.getErrorAttributes(requestAttributes, false);
+        log.info("报错信息:" + JSON.toJSONString(errAttr));
         //解析 VException
         Exception ex = (Exception) request.getAttribute(KEY_EXCEPTION);
         if (ex != null && ex instanceof VException) {
@@ -56,9 +58,7 @@ class GlobalErrController implements ErrorController {
             VException vex = (VException) ex;
             return Results.fail(vex);
         }
-        ServletWebRequest requestAttributes = new ServletWebRequest(request);
-        Map<String, Object> errAttr = errorAttributes.getErrorAttributes(requestAttributes, false);
-        log.info("报错信息:" + JSON.toJSONString(errAttr));
+
         return Results.fail();
     }
 }
