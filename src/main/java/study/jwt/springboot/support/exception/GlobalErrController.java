e60045c471c867dbe37660dbb4d678f0dbf34330
wangzhenjun3 <wangzhenjun3@xdf.cn>
2021-03-01 15:54:17 +0800

    更新代码

--- src/main/java/study/jwt/springboot/support/exception/GlobalErrController.java
+++ src/main/java/study/jwt/springboot/support/exception/GlobalErrController.java
@@ -16,12 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
-/**
- * 1.注解 @ControllerAdvice 方式只能处理控制器抛出的异常，此时请求已经进入控制器中
- * 2.类 ErrorController 方式可以处理所有的异常，包括未进入控制器的错误，比如404,401等错误
- * 3.如果应用中两者共同存在，则 @ControllerAdvice 方式处理控制器抛出的异常，类 ErrorController 方式未进入控制器的异常
- * 4.@ControllerAdvice 方式可以定义多个拦截方法，拦截不同的异常类，并且可以获取抛出的异常信息，自由度更大
- */
 @Slf4j
 @Controller
 class GlobalErrController implements ErrorController {
